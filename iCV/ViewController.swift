c4715d2bfdb5ca4a47a10eb78d3d797d0674d520
Jesus Alexandro Corzo Leon <acorzo83@gmail.com>
2019-08-14 02:03:29 -0500

    Versión Inicial - Test - Mobile Developer - Capgemini

---
+++ iCV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iCV
+//
+//  Created by Jesus Alexandro Corzo Leon on 8/13/19.
+//  Copyright © 2019 Jesus Alexandro Corzo Leon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
